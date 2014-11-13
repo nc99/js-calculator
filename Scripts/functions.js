0c5dd33b19a6da818d0ee1864473dc4c99322479
nc99 <nicolaschan99@gmail.com>
2014-11-13 12:13:14 -0800

    Update functions.js

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -186,7 +186,7 @@ function mandelbrot(arg) {
     
     var c = corner[0];
     
-    while ((c.real < corner[1].real) && (c.imaginary > corner[1].imaginary)) {
+    while ((c.real < corner[1].real) && (c.imaginary > corner[1].imaginary)) {alert("am i looping?");
         var iterations = mandelbrotTest([c]);
 
         output.push(createText((x_dim - (x_dim / (corner[0].real - corner[1].real))) + ((x_dim / (corner[0].real - corner[1].real)) * c.real)));
