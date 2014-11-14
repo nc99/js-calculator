c29ef9da82a59c3c18942e7a1dae78315e0eb2fa
nc99 <nicolaschan99@gmail.com>
2014-11-14 12:34:40 -0800

    Update functions.js

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -215,7 +215,7 @@ function mandelbrot(arg) {
         c.real += (Math.abs(corner[0].real - corner[1].real) / x_dim);
 
         if (c.real >= corner[1].real) {
-            c.imaginary += (corner[1].imaginary * (Math.abs(corner[0].imaginary - corner[1].imaginary) / y_dim));
+            c.imaginary += ((Math.abs(corner[0].imaginary - corner[1].imaginary) / y_dim));
             c.real = corner[0].real;
         }
     }
