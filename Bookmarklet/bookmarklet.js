2b16fce724caa1d3464281cf6ba8cd528b0d6183
Nicolas Chan <nicolaschan99@gmail.com>
2015-01-01 23:16:10 -0800

    fancy

--- Bookmarklet/bookmarklet.js
+++ Bookmarklet/bookmarklet.js
@@ -6,7 +6,7 @@ c.width="40%";
 c.height="30%"; 
 c.frameBorder="0"; 
 c.style.border="2px solid #cccccc"; 
-c.style.position="absolute"; 
+c.style.position="fixed"; 
 c.style.top="20px"; 
 c.style.right="20px"; 
 document.body.appendChild(c); 
